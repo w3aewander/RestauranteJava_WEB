8d9048517767249ce7e053bafc1a63bb7a96205a
w3aewander <wander.silva@gmail.com>
2016-11-09 10:01:21 -0200

    Adicionando classes complementares

--- src/java/com/wander/restaurante/util/Utils.java
+++ src/java/com/wander/restaurante/util/Utils.java
@@ -16,64 +16,18 @@ import java.util.Calendar;
  */
 public class Utils {
 
-    /**
-     * Converte uma string em data
-     *
-     * @param s
-     * @return
-     * @throws ParseException
-     */
-    public static java.util.Date toDBDateFromString(String s) throws ParseException {
-        SimpleDateFormat sf = new SimpleDateFormat("yyyy/MM/dd");
-        java.util.Date d = sf.parse(s);
-        return d;
-    }
-
-    /**
-     * Método sobrecarregado para converter uma String em data
-     *
-     * @param s
-     * @param formato
-     * @return
-     * @throws ParseException
-     */
-    public static java.util.Date toDBDateFromString(String s, String formato) throws ParseException {
-        SimpleDateFormat sf = new SimpleDateFormat(formato);
-        java.util.Date d = sf.parse(s);
-        return d;
-    }
-
-    /**
-     * Converte uma data do banco MySQL para o format string dd/MM/yyyy
-     *
-     * @param d
-     * @return String
-     */
-    public static String fromDBDateToString(java.sql.Date d) {
-        SimpleDateFormat sf = new SimpleDateFormat("dd/MM/yyyy");
-        return sf.format(d);
-    }
-
-    /**
-     * Método sobrecarregado para converter uma data do banco MySQL para o
-     * format string dd/MM/yyyy
-     *
-     * @param d
-     * @param formato
-     * @return String
-     */
-    public static String fromDBDateToString(java.sql.Date d, String formato) {
-        SimpleDateFormat sf = new SimpleDateFormat(formato);
-        return sf.format(d);
-    }
-
-    /**
-     *
-     * @param d
-     * @return
-     */
-    public static java.sql.Date convertJavaDateToSqlDate(java.util.Date d) {
-        return new java.sql.Date(d.getTime());
+    public Timestamp convertForward(Calendar value) {
+        SimpleDateFormat format;
+        format = new SimpleDateFormat("yyyy-MM-dd hh:mm:ss");
+        format.setLenient(false);
+        String stringData = format.format(value.getTime());
+        Timestamp data = null;
+        try {
+            data = new Timestamp(format.parse(stringData).getTime());
+        } catch (ParseException e) {
+            e.printStackTrace();
+        }
+        return data;
     }
 
     public static Calendar convertReverse(Timestamp value) {
@@ -85,7 +39,6 @@ public class Utils {
         try {
             data.setTimeInMillis(format.parse(stringData).getTime());
         } catch (ParseException e) {
-            e.printStackTrace();
         }
         return data;
     }
